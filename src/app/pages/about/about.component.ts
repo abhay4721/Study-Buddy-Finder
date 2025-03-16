df5e8c84241d774ab4aac0c9ffd069a6b7dc869a
Abhayabhi4721 <abhayabhi4721@gmail.com>
2025-03-16 16:24:24 +0530

    added navigation from abot page

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-about',
-  imports: [],
+  standalone: true,
+  imports: [RouterModule],
   templateUrl: './about.component.html',
-  styleUrl: './about.component.css'
+  styleUrls: ['./about.component.css']
 })
-export class AboutComponent {
+export class AboutComponent {}
 
-}
