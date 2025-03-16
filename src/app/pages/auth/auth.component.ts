c37aa50971e4dbdc53c16129ab7bdc51c7dc968a
Abhayabhi4721 <abhayabhi4721@gmail.com>
2025-03-16 17:22:58 +0530

    update auth page

--- src/app/pages/auth/auth.component.ts
+++ src/app/pages/auth/auth.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { trigger, state, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-auth',
-  imports: [],
+  standalone: true,
   templateUrl: './auth.component.html',
-  styleUrl: './auth.component.css'
+  styleUrl: './auth.component.css',
+  animations: [
+    trigger('fadeIn', [
+      state('void', style({ opacity: 0, transform: 'translateY(-10px)' })),
+      transition(':enter', [animate('0.5s ease-out')]),
+    ]),
+  ],
 })
-export class AuthComponent {
-
-}
+export class AuthComponent {}
