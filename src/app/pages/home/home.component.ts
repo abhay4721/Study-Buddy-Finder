63182c314016d956cd8f26dacb54b772463a54ef
abhay4721 <abhayabhi4721@gmail.com>
2025-03-18 15:24:50 +0530

    fix navigation

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  standalone: true,
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  constructor(private router: Router) {}
 
+  goToFindBuddy() {
+    this.router.navigate(['/find-buddy']);
+  }
+
+  goToChat() {
+    this.router.navigate(['/chat']);
+  }
 }
